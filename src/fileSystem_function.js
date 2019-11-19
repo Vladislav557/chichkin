29fe87bcca081f027c60a124e7a62d35026a2a0f
Chinchilla <trololo557@gmail.com>
2019-11-19 20:09:37 +0300

    v0.1.0

--- src/fileSystem_function.js
+++ src/fileSystem_function.js
@@ -2,27 +2,27 @@ import { promises as fs } from 'fs';
 
 import changePackage from './changePackage';
 import cli from './cli_function';
-import { babel } from './template';
+import template from './template';
 
 export default () => {
 
     fs.mkdir('dist')
-        .then(() => console.log("Директория dist создана"))
+        .then(() => console.log("Создано: директория dist"))
         .catch((error) => console.log(error));
 
     fs.mkdir('src/bin', { recursive: true })
         .then(() => {
             fs.writeFile('src/bin/index.js', '')
-                .then(() => console.log('index.js создан'))
+                .then(() => console.log('Создано: файл index.js'))
                 .catch((error) => console.log(error));
         }).catch((error) => console.log(error));
 
     fs.writeFile('.gitignore', 'dist \n node_modules')
-        .then(() => console.log('.gitignore создан'))
+        .then(() => console.log('Создано: файл .gitignore'))
         .catch((error) => console.log(error));
 
-    fs.writeFile('babel.config.js', babel)
-        .then(() => console.log('babel.config.js создан'))
+    fs.writeFile('babel.config.js', template.babel)
+        .then(() => console.log('Создано: файл babel.config.js'))
         .catch((error) => console.log(error));
 
     setTimeout(cli, 1000);
