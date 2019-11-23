dcc9f92f78c2274efe4806dee94c77f0c5987b40
Chinchilla <trololo557@gmail.com>
2019-11-23 10:44:41 +0300

    v0.1.13

--- src/cli_function.js
+++ src/cli_function.js
@@ -11,22 +11,11 @@ export default () => {
     childProcess.exec('git init', (error, stdout, stderr) => {
         cb(error, { stdout, stderr, message: "Инициализирован: git" });
     });
-    // npm install --save-dev @babel/core
-    childProcess.exec('npm install --save-dev @babel/core', (error, stdout, stderr) => {
-        cb(error, { stdout, stderr, message: "Добавленна зависимость: @babel/core"});
+    // npm install --save-dev @babel/core @babel/node @babel/cli @babel/preset-env
+    childProcess.exec('npm install --save-dev @babel/core @babel/node @babel/cli @babel/preset-env', (error, stdout, stderr) => {
+        cb(error, { stdout, stderr, message: "Добавленны зависимости: babel"});
     });
-    // npm install --save-dev @babel/cli
-    childProcess.exec('npm install --save-dev @babel/cli', (error, stdout, stderr) => {
-        cb(error, { stdout, stderr, message: "Добавленна зависимость: @babel/cli"});
-    });
-    // npm install --save-dev @babel/node
-    childProcess.exec('npm install --save-dev @babel/node', (error, stdout, stderr) => {
-        cb(error, { stdout, stderr, message: "Добавленна зависимость: @babel/node"});
-    });
-    // npm install --save-dev @babel/preset-env
-    childProcess.exec('npm install --save-dev @babel/preset-env', (error, stdout, stderr) => {
-        cb(error, { stdout, stderr, message: "Добавленна зависимость: @babel/preset-env"});
-    });
+    
     // npm install --save-dev eslint
     childProcess.exec('npm install --save-dev eslint', (error, stdout, stderr) => {
         cb(error, { stdout, stderr, message: "Добавленна зависимость: eslint" });
